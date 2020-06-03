ebe8987ed0bd7917cd65db620a05c5ffffdaa1de
Goncalo Correia <goncalopcorreia@hotmail.com>
2020-06-03 11:37:54 +0100

    Initial commit- blockchain created

---
+++ Blockchain.py
@@ -0,0 +1,161 @@
+from hashlib import sha256
+import json
+import time
+from Client import Client
+
+
+class Block:
+    def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
+        """
+        Constructor for the `Block` class.
+        :param index: Unique ID of the block.
+        :param transactions: List of transactions.
+        :param timestamp: Time of generation of the block.
+        :param previous_hash: Hash of the previous block in the chain which this block is part of.
+        """
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash  # Adding the previous hash field
+        self.nonce = nonce
+
+    def compute_hash(self):
+        """
+        Returns the hash of the block instance by first converting it
+        into JSON string.
+        """
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        print(block_string)
+        return sha256(block_string.encode()).hexdigest()
+        # return sha256(bytes(self)).hexdigest()
+
+class Blockchain:
+    # difficulty of PoW algorithm
+    difficulty = 2
+
+    def __init__(self):
+        """
+        Constructor for the `Blockchain` class.
+        """
+        self.unconfirmed_transactions = []  # data yet to get into blockchain
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+        genesis_block = Block(0, [], time.time(), "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        """
+        A quick pythonic way to retrieve the most recent block in the chain. Note that
+        the chain will always consist of at least one block (i.e., genesis block)
+        """
+        return self.chain[-1]
+
+    def proof_of_work(self, block):
+        """
+        Function that tries different values of the nonce to get a hash
+        that satisfies our difficulty criteria.
+        """
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification.
+        Verification includes:
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of a latest block
+          in the chain match.
+        """
+        previous_hash = self.last_block.hash
+        # block.compute_hash()
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not Blockchain.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    @staticmethod
+    def is_valid_proof(block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+        print(transaction)
+
+    def mine(self):
+        """
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out proof of work.
+        """
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
+                          previous_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        print(proof)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+
+class Transaction:
+    def __init__(self, client, message, signature):
+        self.client = client
+        self.message = message
+        self.signature = signature
+
+
+bc = Blockchain()
+client1 = Client("testUser", "test")
+
+cl_transaction = '{{"owner": "{}", "message": "{}", "signature": "{}"}}'.format(client1.user,client1.h.hexdigest(), "teste")
+# cl_transaction = Transaction(client1.user, client1.h.hexdigest(), client1.signature)
+bc.add_new_transaction(cl_transaction)
+mine = bc.mine()
+print("block index: " + str(mine))
+
+# encoding = 'latin1'
+# teste = str(client1.signature)[2:-1]
+# teste = client1.signature.decode('GB18030')
+# cl_transaction = '{{"owner": "{}", "message": "{}", "signature": "{}"}}'.format(client1.user,client1.h.hexdigest(), teste)
+# bc.add_new_transaction(json.loads(cl_transaction))
+
+# bc.add_new_transaction(json.loads('''
+# {
+#   "owner": {},
+#   "message": {},
+#   "signature": {}
+# }
+# '''.format(client1.pk, client1.message, client1.signature)))
---
+++ Client.py
@@ -0,0 +1,55 @@
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+import os.path
+from os import path
+
+
+class Client:
+    def __init__(self, user, message):
+        self.message = message.encode()
+        self.user = user
+        self.pk_file = 'keys/' + user + '.pk.pem'
+        self.sk_file = 'keys/' + user + '.sk.pem'
+        if self.keys_exist():
+            self.sign()
+            self.verify()
+
+    def sign(self):
+        key = RSA.import_key(open(self.sk_file).read())
+        self.h = SHA256.new(self.message)
+        self.signature = pkcs1_15.new(key).sign(self.h)
+        print(self.signature)
+
+    def verify(self):
+        key = RSA.import_key(open(self.pk_file).read())
+        h = SHA256.new(self.message)
+        try:
+            pkcs1_15.new(key).verify(self.h, self.signature)
+            print("The signature is valid.")
+        except (ValueError, TypeError):
+            print("The signature is not valid.")
+
+    def keys_exist(self):
+        if not os.path.exists('keys'):
+            os.makedirs('keys')
+        if not path.exists(self.pk_file) or not path.exists(self.sk_file):
+            self.create_keys()
+        return True
+
+    def create_keys(self):
+        key = RSA.generate(2048)
+        private_key = key.export_key()
+        file_out = open(self.sk_file, "wb")
+        file_out.write(private_key)
+        file_out.close()
+
+        public_key = key.publickey().export_key()
+        file_out = open(self.pk_file, "wb")
+        file_out.write(public_key)
+        file_out.close()
+
+cl = Client("djguu", "teste")
+# print(type(cl.message))
+
+
